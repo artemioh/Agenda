fd63c0293239ed25cba6a7b63fe37fce27f5813c
Artemio <artemioh@hotmail.com>
2018-11-04 10:06:32 -0500

    Add new form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MyNavComponent } from './my-nav/my-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule, MatGridListModule, MatCardModule, MatMenuModule, MatTreeModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule, MatGridListModule, MatCardModule, MatMenuModule, MatTreeModule, MatInputModule, MatSelectModule, MatRadioModule } from '@angular/material';
 import { MyTableComponent } from './my-table/my-table.component';
 import { MyDashComponent } from './my-dash/my-dash.component';
 import { MyTreeComponent } from './my-tree/my-tree.component';
 import { MyDdComponent } from './my-dd/my-dd.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { NewFormComponent } from './new-form/new-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,8 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
     MyTableComponent,
     MyDashComponent,
     MyTreeComponent,
-    MyDdComponent
+    MyDdComponent,
+    NewFormComponent
   ],
   imports: [
     BrowserModule,
@@ -39,7 +42,11 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
     MatCardModule,
     MatMenuModule,
     MatTreeModule,
-    DragDropModule
+    DragDropModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
